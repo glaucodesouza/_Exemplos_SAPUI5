0b7a7e19979eb0c13513d5782f2b5d6c1707ca99
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2023-04-07 13:36:46 -0300

    mvc-v2 initial commit

---
+++ mvc-v2/README.md
@@ -0,0 +1,2 @@
+# mvc-v2 SAPUI5 book sap-press chapter 5 page 132
+Example of Codification for JS View
---
+++ mvc-v2/webapp/view/Master.view.js
@@ -0,0 +1,68 @@
+sap.ui.jsview("sapui5.demo.mvcapp.view.Master",{ //attention: sapui5.demo.mvcapp is the namespace for webapp folder, not the complete file path...
+    getControllerName: function(){
+        return "sapui5.demo.mvcapp.controller.Master";
+    },
+    createContent: function(oController){
+        //here we will create our UI via JS Coding...
+        // 3-TABLE COLUMNS the columns will act as column readers
+        var aColums = [
+            new sap.m.Column({
+            header : new sap.m.Text({
+                text : "ID"
+            })
+            }),
+            new sap.m.Column({
+            header : new sap.m.Text({
+                text : "Name"
+            })
+            })
+        ];
+
+        // 4-TEMPLATE in the Template, we will display the supplier information
+        var oTemplate = new sap.m.ColumnListItem({
+            type: "Navigation",
+            cells: [
+            new sap.m.ObjectIdentifier({
+                text: "{ID}"
+            }),
+            new sap.m.ObjectIdentifier({
+                text: "{Name}"
+            })
+            ]
+        });
+
+        // 5-HEADER in the header, we are displaying the number of suppliers
+        var oTableHeader = new sap.m.Toolbar({
+            content: [
+            new sap.m.Title({
+                text: "Number of suppliers {/CountSuppliers}"
+            })
+            ]
+        });
+
+        // 6-TABLE we create the table with the columns and header
+        var oTable = new sap.m.Table({
+            columns: aColums,
+            headerToolbar: oTableHeader
+        });
+
+        // 7-BIND we bind the table items to the /Suppliers entries
+        // and to the template
+        oTable.bindItems("/Suppliers", oTemplate);
+
+        // 8-PAGE
+        // var oPageMaster = new sap.m.Page("masterPage",{
+        var oPageMaster = new sap.m.Page({ //new line here, comparing to mvc-v1 example...
+            title: "Supplier Overview",
+            content: [oTable]
+        });
+
+        var oApp = new sap.m.App("myApp");
+
+        oApp.addPage(oPageMaster);
+
+        oApp.placeAt("content");
+
+        return oPageMaster; //new line here, comparing to mvc-v1 example...
+    }
+});
